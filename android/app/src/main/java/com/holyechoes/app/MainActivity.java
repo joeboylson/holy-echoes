dbdf7a22b03125443ae3d2d90e8600ec204056e7
Joe Boylson <joeboylson@gmail.com>
2025-06-22 15:12:49 -0400

    update codemagic.yml

--- android/app/src/main/java/com/holyechoes/app/MainActivity.java
+++ android/app/src/main/java/com/holyechoes/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.app;
+package com.holyechoes.app;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from "@capacitor/cli";
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: "com.example.app",
-  appName: "com.holyechoes.app",
-  webDir: "dist",
+  appId: 'com.holyechoes.app',
+  appName: 'holy-echoes',
+  webDir: 'dist'
 };
 
 export default config;
