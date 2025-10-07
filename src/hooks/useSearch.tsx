148a46b42797d4d904bf2dcbd79b855dfdfb4624
Joe Boylson <joeboylson@gmail.com>
2025-10-06 21:44:11 -0400

    feat: fix build and lint errors

--- src/hooks/useSearch.tsx
+++ src/hooks/useSearch.tsx
@@ -11,6 +11,7 @@ export default function useSearch(searchTerm: string) {
       ? {
           prayers: {
             prayerBlocks: {
+              blockType: {},
               litanyBlocks: {},
             },
             $: {
